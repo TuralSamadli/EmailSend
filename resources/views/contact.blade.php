cff9f5046abc4a960529fb9589cfa13739660a2e
Tural Samadli <tsamadli4696@ada.edu.az>
2022-08-12 17:08:48 +0400

    ajax done

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,7 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Users</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
     <table style="solid black">
@@ -18,16 +21,19 @@
         <tr>
           <td>{{$user->name}}</td>
           <td>{{$user->email}}</td> 
-          <td>  <a href= "{{route('mail')}}"    type="button">> Send Mail </a></td>
+          <td> <button onclick="SendEmail('{{$user->email}}')" type="button" class="btn waves-effect waves-light btn-danger">Send</button></td>
         </tr>
         
       </table>
     
 </body>
+
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
 
 <script>
-    function Mail(id) {
+    function SendEmail(email) {
+        console.log(email)
         swal({
             title: "Warning",
             text: "Are you sure?",
@@ -36,19 +42,18 @@
             dangerMode: true,
             buttons: ["No", "Yes"],
         })
-            .then((SendMessage) => {
-                if (willDelete) {
-                    let CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
+            .then((willSend) => {
+                if (willSend) {
                     $.ajax({
-                        url:  {{route('mail')}},
+                        url: "{{ route('mail') }}",
                         data: { "_token": "{{ csrf_token() }}", email:email },
-                        method: "POST",
+                        type: "POST",
                         success: function (data) {
                             if(data==="ok"){
-                                swal("Success!", "Mail sent!", "success");
+                                swal("Success!", "Email Sent!", "success");
                                 window.setTimeout(function(){location.reload()},2000)
                             }else{
-                                swal("Error!", "Mail did not send!", "error");
+                                swal("Error!", "Email didn't sent!", "error");
                             }
                         },
                         error: function (x, sts) {
